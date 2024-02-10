4d754db55d175e7c6572eb3d057f2f1bc71e303c
Deno Rautenbach <denorauten@gmail.com>
2024-02-10 06:16:42 +0200

    first commit

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,13 @@
+const HeroSection = () => {
+    return (
+      <section className="container hero bg-white">
+        <div className="grid lg:grid-cols-3 gap-3">
+          <div className="bg-red-600">Welcome to My Website</div>
+          <div className="bg-purple-900">Welcome to My Website</div>
+          <div className="bg-green-600">Welcome to My Website</div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default HeroSection;
